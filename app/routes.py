87139f25b9f5dffbd267d1e05bf7e437b9c47882
Lovkush Agarwal <lovkush@gmail.com>
2023-10-29 09:17:05 +0000

    adjust to get first web app runniing locally

--- app/routes.py
+++ app/routes.py
@@ -2,15 +2,29 @@ from flask import render_template, request, redirect, url_for
 from app import app
 
 # Import the OpenAI library here
+import openai
+
+# Load the OpenAI API key from the .env file
+import os
+from dotenv import load_dotenv
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         user_input = request.form['user_input']
-        
-        # Make a call to the OpenAI API here
+
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a software engineering expert and excellent at explaining complex programming concepts in simple terms."},
+                {"role": "user", "content": user_input}
+            ]
+        )
         
         # Process the API response and store the result
+        result = completion.choices[0].message
         
         return render_template('result.html', result=result)
     
